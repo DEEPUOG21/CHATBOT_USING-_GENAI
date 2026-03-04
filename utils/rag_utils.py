43ff92714dd4b21a29fcb1d70f012942a679027d
DEEPUOG21 <deepusaideep21@gmail.com>
2026-03-04 17:08:40 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- utils/rag_utils.py
+++ utils/rag_utils.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from models.embeddings import get_embedding_model
 
